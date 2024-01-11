2e1fa8e8f961ebc49e358187e74085ec8b3c257d
chocomacaroon <156282117+chocomacaroon@users.noreply.github.com>
2024-01-12 00:20:01 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,14 @@
+fir, sec, thr = map(int, input().split())
+if(fir == sec == thr):
+    prize = 10000 + fir * 1000
+elif(fir != sec and sec != thr and fir != thr):
+  if(fir > sec): max = fir
+  elif(sec > fir): max = sec
+  if(thr > max): max = thr
+  prize = max * 100
+else:
+    if(fir == sec or fir == thr):
+        prize = 1000 + fir * 100
+    else:
+        prize = 1000 + sec * 100
+print(prize)
