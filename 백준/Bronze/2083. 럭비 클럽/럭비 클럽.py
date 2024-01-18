51ae4345dd31f1eb6a08cea320fcf9523f41c54f
chocomacaroon <156282117+chocomacaroon@users.noreply.github.com>
2024-01-18 12:55:49 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,10 @@
+while True:
+  name, age, weight = input().split()
+  age = int(age)
+  weight = int(weight)
+  if name == "#" and age == 0 and weight == 0: break
+  if age > 17 or weight >= 80:
+    category = "Senior"
+  else:
+    category = "Junior"
+  print(name, category)
