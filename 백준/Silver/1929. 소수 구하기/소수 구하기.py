96040caea41592aab02fb3797dc9ccead72c83a7
chocomacaroon <156282117+chocomacaroon@users.noreply.github.com>
2024-02-01 00:59:47 +0900

    [Silver III] Title: 소수 구하기, Time: 584 ms, Memory: 41492 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,14 @@
+import math
+
+M, N = map(int, input().split())
+arr = [True for i in range(N+1)]
+arr[1] = False
+for i in range(2, int(math.sqrt(N))+1):
+    if arr[i] == True:
+        j = 2
+        while i * j <= N:
+            arr[i*j] = False
+            j+=1
+for i in range(M, N+1):
+  if arr[i]:
+      print(i)
