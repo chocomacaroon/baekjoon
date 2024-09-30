233925d9fe04292880b0c13d87ee32c82e1b717b
chocomacaroon <156282117+chocomacaroon@users.noreply.github.com>
2024-09-30 09:58:27 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.54 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -0,0 +1,6 @@
+def solution(my_string, queries):
+    answer = ''
+    for q in queries:
+        a,b = q
+        my_string = my_string[:a]+my_string[a:b+1][::-1]+my_string[b+1:]
+    return my_string
