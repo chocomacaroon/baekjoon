28dd521caf41fa46b37db1bfdd99a49c663427ab
chocomacaroon <156282117+chocomacaroon@users.noreply.github.com>
2024-11-01 03:05:47 +0900

    [Bronze I] Title: A/B - 2, Time: 40 ms, Memory: 31252 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,2 +1,10 @@
-n, m = map(int, input().split())
-print(round(n/m,2000))
+n, m = map(int,input().split())
+answer = 0
+print(n//m,end="")
+if n%m:
+    print(".",end="")
+    i = 0
+    while n%m and i < 1000:
+        n = n % m * 10
+        i += 1
+        print(n // m, end = "")
