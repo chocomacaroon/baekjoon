dea689a9e8fe091b40e18380a5b7f42c957b1b5d
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-02-15 23:43:43 +0900

    [level 0] Title: k의 개수, Time: 6.52 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int i, int j, int k) {
+    int answer = 0;
+    for(int n = i; n <= j; n++){
+        for(auto c:to_string(n)){
+            if(c-'0' == k){
+                answer++;
+            }
+        }
+    }
+    return answer;
+}
