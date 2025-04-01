3f54157736263d151dc710975006ecff55b9049d
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-01 21:27:16 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 2.88 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.cpp
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int main(void) {
     int n;
     cin >> n;
-    for(int i = 1; i <= n; i++){
-        for(int j = 0; j < i; j++)
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i+1; j++){
             cout << "*";
+        }
         cout << endl;
     }
+        
     return 0;
 }
