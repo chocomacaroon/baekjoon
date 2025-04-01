31f6a401038bfa14d816c529bba8d405e1980d12
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-01 20:11:28 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -5,9 +5,11 @@ using namespace std;
 
 int solution(int num1, int num2) {
     int answer = 0;
-    if (num1 == num2)
-        answer = 1;
-    else
-        answer = -1;
+    if(num1==num2){
+        return 1;
+    }
+    else{
+        return -1;
+    }
     return answer;
 }
