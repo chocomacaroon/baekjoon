40974d9bad45b7b1ab1d475df0bf220537b3381e
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-02 09:48:09 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.cpp
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 string solution(string cipher, int code) {
     string answer = "";
-    for(int i = code-1;i < cipher.size(); i+=code){
+    for(int i = code-1 ; i < cipher.size(); i+=code){
         answer += cipher[i];
     }
     return answer;
