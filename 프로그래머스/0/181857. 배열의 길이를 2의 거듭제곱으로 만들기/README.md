75d11876fc9b18b0f566faed21ce1880471a0bb0
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-03 01:10:42 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.27 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/README.md
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열의 길이를 2의 거듭제곱으로 만들기 - 181857 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181857) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181857?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 0.37 ms
+메모리: 3.82 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 14일 14:01:50
+2025년 04월 03일 01:10:40
 
 ### 문제 설명
 
--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.cpp
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.cpp
@@ -5,21 +5,14 @@ using namespace std;
 
 vector<int> solution(vector<int> arr) {
     vector<int> answer;
-    
     int n = 1;
-    
-    while(1){
-       if(n >= arr.size()){
-           break;
-       }
+    while(n < arr.size()){
         n *= 2;
-   }
-    
-    for(int i = 0; i < arr.size(); i++){
-        answer.push_back(arr[i]);
     }
-   
-    for(int i = 1; i <= n-arr.size();i++){
+    for(auto num:arr){
+        answer.push_back(num);
+    }
+    for(int i = arr.size(); i < n; i++){
         answer.push_back(0);
     }
     return answer;
