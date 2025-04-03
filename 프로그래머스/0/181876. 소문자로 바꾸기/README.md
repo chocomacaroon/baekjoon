6ffef937a4a0d9c6ff74cb0cbe79a91461840896
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-03 10:50:12 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/0/181876. 소문자로 바꾸기/README.md
+++ 프로그래머스/0/181876. 소문자로 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 소문자로 바꾸기 - 181876 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181876) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181876?language=cpp) 
 
 ### 성능 요약
 
-메모리: 9.2 MB, 시간: 0.00 ms
+메모리: 3.63 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 18:55:34
+2025년 04월 03일 10:50:10
 
 ### 문제 설명
 
--- 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -1,12 +1,12 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
 
 string solution(string myString) {
     string answer = "";
-    for(int i = 0; i < myString.size(); i++){
-        answer += tolower(myString[i]);
-    }
-    return answer;
+    transform(myString.begin(), myString.end(), myString.begin(),::tolower);
+    return myString;
 }
