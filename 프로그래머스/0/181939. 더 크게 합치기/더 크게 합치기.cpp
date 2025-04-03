ae101e2b323bd47b2d108e291cd2438b04243fb7
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-03 15:37:16 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 int solution(int a, int b) {
-    if(stoi(to_string(a)+to_string(b)) > stoi(to_string(b)+to_string(a))){
+    int answer = 0;
+    if(to_string(a)+to_string(b) > to_string(b)+to_string(a)){
         return stoi(to_string(a)+to_string(b));
     }
     else{
         return stoi(to_string(b)+to_string(a));
     }
+    return answer;
 }
