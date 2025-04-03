9c346a509122b95e8bfe090a373e2f80d07f1f9c
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-03 11:25:01 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/README.md
+++ 프로그래머스/0/181905. 문자열 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 뒤집기 - 181905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905?language=cpp) 
 
 ### 성능 요약
 
-메모리: 3.68 MB, 시간: 0.02 ms
+메모리: 4.02 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 14일 16:27:39
+2025년 04월 03일 11:24:59
 
 ### 문제 설명
 
--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -6,10 +6,9 @@ using namespace std;
 
 string solution(string my_string, int s, int e) {
     string answer = "";
-    answer += my_string.substr(0,s);
-    string tmp = my_string.substr(s,e-s+1);
-    reverse(tmp.begin(), tmp.end());
-    answer += tmp;
-    answer += my_string.substr(e+1,my_string.size()-e+1);
+    string rev = my_string.substr(s,e-s+1);
+    reverse(rev.begin(),rev.end());
+    answer = my_string.substr(0,s) + rev + my_string.substr(e+1,my_string.size());
+    
     return answer;
 }
