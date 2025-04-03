487155918add7dab60be01eb203700cd0def5a28
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-03 13:37:58 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/README.md
+++ 프로그래머스/0/181891. 순서 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 순서 바꾸기 - 181891 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181891) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181891?language=cpp) 
 
 ### 성능 요약
 
-메모리: 9.2 MB, 시간: 0.00 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 19:53:49
+2025년 04월 03일 13:37:56
 
 ### 문제 설명
 
--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
@@ -1,15 +1,13 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> num_list, int n) {
     vector<int> answer;
-    for(int i = n; i < num_list.size(); i++){
-        answer.push_back(num_list[i]);
-    }
     for(int i = 0; i < n; i++){
-        answer.push_back(num_list[i]);
+        rotate(num_list.begin(), num_list.begin()+1, num_list.end());
     }
-    return answer;
+    return num_list;
 }
