85005097d614a20ee074c1bd95114e5a48cfc378
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-09 16:09:07 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,11 +1,3 @@
 def solution(numlist, n):
-    answer = []
-    tmp = []
-    numlist.sort(reverse=True)
-    for c in numlist:
-        tmp.append(abs(c-n))
-    sort = sorted(tmp)
-    for a in sort:
-        answer.append(numlist[tmp.index(a)])
-        tmp[tmp.index(a)] = -1
+    answer = sorted(numlist,key = lambda x : (abs(x-n), -x))
     return answer
