e94a317af1ddbd5003b45973b11fce59db5850a6
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-04-11 17:55:04 +0900

    [level 2] Title: 구명보트, Time: 25.00 ms, Memory: 9.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,34 +1,17 @@
-# def solution(people, limit):
-#     answer = 0
-#     people = sorted(people,reverse=True)
-#     i=0
-#     j=len(people)-1
-#     while True:
-#         if people[i]+people[j] <= limit:
-#             i+=1
-#             j-=1
-#             answer+=1
-#         else:
-#             if j-i==1:
-#                 return answer+2
-#             answer+=1
-#             i+=1
-#         if i >= j:
-#             return answer
-#     return answer
+from collections import deque
 
 def solution(people, limit):
     answer = 0
-    people = sorted(people, reverse=True)
-    i = 0
-    j = len(people) - 1
-
-    while i <= j:
-        # 가장 무거운 사람과 가장 가벼운 사람의 합이 limit 이하일 때
-        if people[i] + people[j] <= limit:
-            j -= 1  # 가장 가벼운 사람도 태움
-        # 가장 무거운 사람만 태울 경우
-        i += 1
-        answer += 1
-
+    people.sort(reverse=True)
+    stk = deque(people)
+    while stk:
+        if len(stk) >= 2 and (stk[0]+stk[-1] <= limit):
+            answer+=1
+            stk.popleft()
+            stk.pop()
+        else:
+            stk.popleft()
+            answer+=1
     return answer
+
+#80 70 50 50
