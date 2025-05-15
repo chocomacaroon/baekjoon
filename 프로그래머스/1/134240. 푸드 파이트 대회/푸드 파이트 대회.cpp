93b375954e93013962f9e6201f4e678d7f053ea2
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-05-15 23:11:23 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.29 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    string tmp = "";
+    for(int i = 1; i < food.size(); i++){
+        for(int j = 0; j < food[i]/2; j++)
+            tmp += to_string(i);
+    }
+    answer = tmp + "0";
+    reverse(tmp.begin(), tmp.end());
+    answer += tmp;
+    return answer;
+}
