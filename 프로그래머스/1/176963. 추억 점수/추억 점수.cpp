3422a1ae9d1623bbc6999cb3e68a622ced827ff7
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-05-16 01:05:59 +0900

    [level 1] Title: 추억 점수, Time: 2.06 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -6,14 +6,13 @@ using namespace std;
 
 vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
     vector<int> answer;
-    for(auto p:photo){
-        int res = 0;
-        for(auto n:p){
-            if(find(name.begin(), name.end(), n) != name.end()){
-                res += yearning[distance(name.begin(), find(name.begin(), name.end(), n))];
-            }
+    for(int i = 0; i < photo.size(); i++){
+        int sum = 0;
+        for(auto n : photo[i]){
+            if(find(name.begin(), name.end(),n)!=name.end())
+                sum += yearning[find(name.begin(), name.end(),n)-name.begin()];
         }
-        answer.push_back(res);
+        answer.push_back(sum);
     }
     return answer;
 }
