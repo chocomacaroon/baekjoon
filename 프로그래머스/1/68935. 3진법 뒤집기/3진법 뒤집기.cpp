0ab95cab2fa6b33d092bb819a4c3c1a47d84abba
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-06-25 17:05:30 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -1,24 +1,21 @@
 #include <string>
 #include <vector>
-#include <cmath>
 #include <algorithm>
-#include <iostream>
+#include <cctype>
+#include <cmath>
 
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    string thr = "";
+    string s_a = "";
     while(n>0){
-        thr += to_string(n%3);
+        s_a += to_string(n%3);
         n /= 3;
     }
-    // answer = stoi(thr);
-    // return answer;
-    reverse(thr.begin(), thr.end());
-    // return stoi(thr);
-    for(int i = 0; i < thr.size(); i++){
-        answer += (thr[i]-'0')*(pow(3,i));
+    reverse(s_a.begin(), s_a.end());
+    for(int i = 0; i < s_a.size(); i++){
+        answer += ((s_a[i]-'0'))*(pow(3,i));
     }
     return answer;
 }
