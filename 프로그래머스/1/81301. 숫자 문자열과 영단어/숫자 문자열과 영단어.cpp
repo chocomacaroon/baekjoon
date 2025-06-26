3214d6c95cc435ec5619543f85ebcace430d2a07
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-06-26 09:34:26 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,24 +1,23 @@
 #include <string>
 #include <vector>
-#include <cctype>
 #include <algorithm>
 
 using namespace std;
 
 int solution(string s) {
     int answer = 0;
+    vector<string> d = {"zero", "one","two","three","four", "five","six","seven","eight","nine"};
     string tmp = "";
-    vector<string> number{"zero","one","two","three","four","five","six","seven","eight","nine"};
     for(int i = 0; i < s.size(); i++){
-        if(isdigit(s[i])){
-            answer = answer*10+s[i]-'0';
-        }
-        else{
+        if(isalpha(s[i])){
             tmp += s[i];
+            if(find(d.begin(), d.end(),tmp)!=d.end()){
+                answer = answer * 10 + find(d.begin(), d.end(), tmp) - d.begin();
+                tmp = "";
+            }
         }
-        if(find(number.begin(), number.end(), tmp) != number.end()){
-            answer = answer*10+distance(number.begin(), find(number.begin(), number.end(), tmp));
-            tmp = "";
+        else if(isdigit(s[i])){
+            answer = answer * 10 + (s[i]-'0');
         }
     }
     return answer;
