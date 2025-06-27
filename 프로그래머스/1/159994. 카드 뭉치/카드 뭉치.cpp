b4c80715e9b452d4067f759e8955ba2ae5bd910e
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-06-27 09:09:53 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -4,19 +4,20 @@
 using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
-    string answer = "";
-    int idx1 = 0;
-    int idx2 = 0;
+    string answer = "Yes";
+    int a = 0;
+    int b = 0;
+    
     for(int i = 0; i < goal.size(); i++){
-        if(cards1.front() == goal[i]){
-            cards1.erase(cards1.begin());
+        if(cards1[a] == goal[i]){
+            a+=1;
         }
-        else if(cards2.front() == goal[i]){
-            cards2.erase(cards2.begin());
+        else if(cards2[b] == goal[i]){
+            b+=1;
         }
         else{
             return "No";
         }
     }
-    return "Yes";
+    return answer;
 }
