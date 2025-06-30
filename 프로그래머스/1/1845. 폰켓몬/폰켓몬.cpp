8b3df35c119933bc91f91f921531370db0391085
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-06-30 11:52:02 +0900

    [level 1] Title: 폰켓몬, Time: 0.70 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -6,9 +6,12 @@ using namespace std;
 int solution(vector<int> nums)
 {
     int answer = 0;
-    int limit = nums.size()/2;
+    int sz = nums.size()/2;
     sort(nums.begin(), nums.end());
-    nums.erase(unique(nums.begin(), nums.end()),nums.end());
-    return (limit<nums.size())?limit:nums.size();
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    if(sz > nums.size())
+        answer = nums.size();
+    else
+        answer = sz;
     return answer;
 }
