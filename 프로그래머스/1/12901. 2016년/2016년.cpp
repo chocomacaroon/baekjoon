33f9a68de84ebd560148268c2b71b3347753812f
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-06-30 11:59:36 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -2,14 +2,13 @@
 #include <vector>
 
 using namespace std;
-
+vector<int> days = {0,31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+vector<string> res = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
 string solution(int a, int b) {
     string answer = "";
-    int days = b;
-    vector<int> m_days({31,29,31,30,31,30,31,31,30,31,30,31});
-    vector<string> day({"MON","TUE","WED","THU","FRI","SAT","SUN"});
-    for(int i = 0; i < a-1; i++){
-        days+=m_days[i];
+    int tday = b;
+    for(int i = 1; i < a; i++){
+        tday += days[i];
     }
-    return day[(days+3)%7];
+    return res[(tday+4)%7];
 }
