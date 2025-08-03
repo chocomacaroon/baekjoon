4f12d3c0d9c20e757d833b29d0ab073263ff84d4
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-03 14:30:12 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.09 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 string solution(int n) {
     string answer = "";
-    for(int i = 0; i < n/2;i++){
+    for(int i = 0; i < n/2; i++){
         answer += "수박";
     }
-    if(n%2==1){
-        answer+="수";
-    }
+    if(n%2) answer+="수";
     return answer;
 }
