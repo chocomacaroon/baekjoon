5e56137e100f3ae3602e5111a1f0906e5bd7c3e3
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-03 14:57:25 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.cpp
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -7,14 +7,13 @@
 using namespace std;
 
 int solution(vector<int> d, int budget) {
-    int answer = 0;
-    int sum = 0;
+    int answer = d.size();
+    int num = 0;
     sort(d.begin(), d.end());
-    for(int i = 0; i < d.size(); i++){
-        sum += d[i];
-        if(sum > budget){
-            return i;
-        }
+    for(int i = 0; i < d.size(); i++)
+    {
+        num += d[i];
+        if(num > budget) return i;
     }
-    return d.size();
+    return answer;
 }
