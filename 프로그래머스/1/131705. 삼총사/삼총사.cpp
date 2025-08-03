c78184d865e88616d92cf5555c5653e134746005
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-03 14:51:12 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -7,8 +7,8 @@ int solution(vector<int> number) {
     int answer = 0;
     for(int i = 0; i < number.size()-2; i++)
         for(int j = i+1; j < number.size()-1; j++)
-            for(int k = j+1; k < number.size(); k++)
-                if(number[i]+number[j]+number[k] == 0)
-                    answer+=1;
+            for(int k = j+1; k < number.size(); k++){
+                if(number[i]+number[j]+number[k] == 0) answer+=1;
+            }
     return answer;
 }
