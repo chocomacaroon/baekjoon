eac5266c6881785705abc12a466159f8b93c4244
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-04 21:24:58 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -5,21 +5,14 @@ using namespace std;
 
 string solution(string s) {
     string answer = "";
-    int idx = 0;
+    int j = 0;
     for(int i = 0; i < s.size(); i++){
-        if(s[i]==' '){
-            answer += ' ';
-            idx = 0;
+        j+=1;
+        if(s[i] == ' '){
+            j=0; 
         }
-        else{
-            idx+=1;
-            if(idx%2==0){
-            answer += tolower(s[i]);
-        }
-        else if(idx%2!=0){
-            answer += toupper(s[i]);
-        }
-        }
+        if(j%2) answer += toupper(s[i]);
+        else answer += tolower(s[i]);
         
     }
     return answer;
