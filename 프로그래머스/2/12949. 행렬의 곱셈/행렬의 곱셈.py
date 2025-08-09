fd5c38b99c86b1a3e737fc2e4fac99109440b5e2
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-09 16:17:32 +0900

    [level 2] Title: 행렬의 곱셈, Time: 63.18 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,19 +1,11 @@
-#numpy append
-import numpy as np
-
 def solution(arr1, arr2):
     answer = []
-    n1 = len(arr2)
-    m1 = len(arr2[0])
-    new_arr2 = []
-    for j in range(m1):
-        tmp1 = [arr2[i][j] for i in range(n1)]
-        new_arr2.append(tmp1)
-    arr1 = np.array(arr1)
-    new_arr2 = np.array(new_arr2)
-    tmp = []
-    for n in arr1:
-        tmp = [int(np.dot(n,m)) for m in new_arr2]
-        answer.append(tmp)        
+    for x1 in range(len(arr1)):
+        tmp = []
+        for y1 in range(len(arr2[0])):
+            su = 0
+            for y2 in range(len(arr1[0])):
+                su += arr1[x1][y2] *arr2[y2][y1]
+            tmp.append(su)
+        answer.append(tmp)
     return answer
-    
