4779c39dacc955e4ba8700b110b07c9ad9e7ebb8
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-09 12:29:26 +0900

    [level 2] Title: 짝지어 제거하기, Time: 91.39 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=python3) 
 
 ### 성능 요약
 
-메모리: 6.52 MB, 시간: 5.71 ms
+메모리: 11.4 MB, 시간: 91.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 14일 17:17:13
+2025년 08월 09일 12:29:25
 
 ### 문제 설명
 
--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,10 +1,14 @@
 def solution(s):
-    answer = -1
     stk = []
     for c in s:
-        if not stk or (stk and stk[-1]!=c):
+        if not stk:
             stk.append(c)
         else:
-            while stk and stk[-1]==c:
+            if stk[-1] == c:
                 stk.pop()
-    return 0 if stk else 1
+            else:
+                stk.append(c)
+    if stk:
+        return 0
+    else:
+        return 1
