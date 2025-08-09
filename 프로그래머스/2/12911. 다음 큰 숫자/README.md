d62786bffaa2ca55723957c36a6f379fddb0be8b
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-09 12:44:56 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/README.md
+++ 프로그래머스/2/12911. 다음 큰 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다음 큰 숫자 - 12911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911?language=python3) 
 
 ### 성능 요약
 
-메모리: 3.8 MB, 시간: 0.02 ms
+메모리: 9.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 14일 17:14:30
+2025년 08월 09일 12:44:54
 
 ### 문제 설명
 
--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,17 +1,9 @@
 def solution(n):
     answer = 0
-    stand = to_2(n).count('1')
+    target = bin(n)[2:].count('1')
     while True:
         n += 1
-        if to_2(n).count('1') == stand:
+        new = bin(n)[2:]
+        if new.count('1') == target:
             return n
     return answer
-
-def to_2(n):
-    result = ''
-    while n > 0:
-        result += str(n%2)
-        n//=2
-    if n%2==1:
-        result += str(n%2)
-    return result[::-1]
