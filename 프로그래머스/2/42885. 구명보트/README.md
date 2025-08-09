c8fa2c4b5d458417ceb07937dcd2a98835aa05b3
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-09 13:01:03 +0900

    [level 2] Title: 구명보트, Time: 9.85 ms, Memory: 9.65 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.73 MB, 시간: 1.81 ms
+메모리: 9.65 MB, 시간: 9.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 14일 23:11:47
+2025년 08월 09일 13:01:01
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,17 +1,14 @@
-from collections import deque
-
 def solution(people, limit):
     answer = 0
-    people.sort(reverse=True)
-    stk = deque(people)
-    while stk:
-        if len(stk) >= 2 and (stk[0]+stk[-1] <= limit):
-            answer+=1
-            stk.popleft()
-            stk.pop()
+    l = 0
+    r = len(people) - 1
+    people.sort()
+    while l <= r:
+        if people[l] + people[r] <= limit:
+            answer += 1
+            l += 1
+            r -= 1
         else:
-            stk.popleft()
-            answer+=1
+            answer += 1
+            r -= 1
     return answer
-
-#80 70 50 50
