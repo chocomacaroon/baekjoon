eaa70153a82c21f89e8e8508fa69fca24d71ade3
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-09 17:50:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 24.21 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.04 MB, 시간: 0.46 ms
+메모리: 9.35 MB, 시간: 24.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 21일 16:09:57
+2025년 08월 09일 17:50:10
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,21 +1,25 @@
 from collections import deque
 
 def solution(maps):
-    answer = 0
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
-    v = [[0]*len(maps[0]) for _ in range(len(maps))]
+    
+    visited = [[False]*len(maps[0]) for i in range(len(maps))]
+    visited[0][0] = True
+    
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    
+    
     q = deque()
-    q.append((0,0))
-    v[0][0] = 1
+    q.append((0,0, 1))
+    
     while q:
-        x,y=q.popleft()
+        nx, ny, dis = q.popleft()
+        if nx == len(maps)-1 and ny == len(maps[0])-1:
+            return dis
         for i in range(4):
-            nx,ny = x+dx[i],y+dy[i]
-            if 0<=nx<len(maps) and 0<=ny<len(maps[0]):
-                if v[nx][ny]==0 and maps[nx][ny]==1:
-                    q.append((nx,ny))
-                    v[nx][ny] = v[x][y]+1
-                    if nx == len(maps)-1 and ny == len(maps[0])-1:
-                        return v[nx][ny]
+            if nx + dx[i] >= 0 and nx + dx[i] < len(maps) and ny + dy[i] >= 0 and ny + dy[i] < len(maps[0]):
+                if maps[nx + dx[i]][ny + dy[i]] == 1 and not visited[nx + dx[i]][ny + dy[i]]:
+                    visited[nx + dx[i]][ny + dy[i]] = True
+                    q.append((nx + dx[i],ny + dy[i], dis+1))
+
     return -1
