abe66d6256243271a576ea3f6477842784f63ef6
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-21 16:46:39 +0900

    [level 2] Title: 피보나치 수, Time: 0.87 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 int solution(int n) {
-    vector<int> v = {0, 1};
     int answer = 0;
-    if(n == 0 || n == 1) return n;
-    for(int i = 2; i <= n; i++){
-        v.push_back((v[i-1]+v[i-2])%1234567);
+    vector<int> v;
+    v.push_back(0);
+    v.push_back(1);
+    for (int i = 2; i <= n; i++){
+        v.push_back((v[i-1] + v[i-2])%1234567);
     }
-    return v.back()%1234567;
+    return v[n];
 }
