8b12b2c0452358aa5c7d82a9851850be56776cd0
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-21 17:50:38 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.17 MB, 시간: 0.04 ms
+메모리: 3.67 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 16일 00:13:15
+2025년 08월 21일 17:50:35
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -1,22 +1,23 @@
 #include <string>
 #include <vector>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
-    vector<int> answer = {0, 0};
-    vector<string> box;
-    box.push_back(words[0]);
+    vector<int> answer;
+    vector<string> before;
+    
+    before.push_back(words[0]);
     
     for(int i = 1; i < words.size(); i++){
-        if(find(box.begin(), box.end(), words[i])!=box.end() || words[i][0] != box[i-1][box[i-1].size()-1]){
+        if (find(before.begin(), before.end(), words[i]) == before.end() && before.back().back() == words[i][0]){
+            before.push_back(words[i]);
+        }
+        else{
             return {i%n+1, i/n+1};
         }
-        
-        box.push_back(words[i]);
-        
     }
-
-    return answer;
+    return {0,0};
 }
