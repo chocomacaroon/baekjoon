657bc67586eaea4e0d7b8b76acd0dcf0b0e4be1c
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-21 21:33:07 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 204.32 ms, Memory: 23.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=cpp) 
 
 ### 성능 요약
 
-메모리: 23.2 MB, 시간: 198.61 ms
+메모리: 23.3 MB, 시간: 204.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 16일 11:24:08
+2025년 08월 21일 21:33:05
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,6 +1,6 @@
 #include <string>
-#include <set>
 #include <vector>
+#include <set>
 #include <algorithm>
 #include <numeric>
 
@@ -9,23 +9,18 @@ using namespace std;
 int solution(vector<int> elements) {
     int answer = 0;
     set<int> s;
-    for(int i = 1; i <= elements.size(); i++){ //i개의 합
+    for(int i = 1; i <= elements.size(); i++){
         int sum = 0;
         for(int j = 0; j < elements.size(); j++){
-            
             if(j+i >= elements.size()){
                 sum = accumulate(elements.begin()+j, elements.end(), 0);
-                sum += accumulate(elements.begin(), elements.begin()+(j+i-elements.size()),0);
+                sum += accumulate(elements.begin(), elements.begin() + (i+j)-elements.size(), 0);
             }
             else{
-                sum = accumulate(elements.begin()+j, elements.begin()+(j+i), 0);
+                sum = accumulate(elements.begin()+j, elements.begin()+i+j, 0);     
             }
             s.insert(sum);
         }
     }
     return s.size();
 }
-
-// 7 9 1 1 4
-// 16 10 2 5 11
-//
