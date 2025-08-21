2e53de081e8c107c81682d8692102e9f677228d7
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-21 17:34:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -4,15 +4,12 @@
 using namespace std;
 
 long long solution(int n) {
-    if(n == 0 || n == 1) return 1;
-
-    vector<long long> dp(n + 1, 0);
+    long long answer = 0;
+    vector<long long> dp(n+1,0);
     dp[0] = 1;
     dp[1] = 1;
-
-    for(int i = 2; i <= n; ++i){
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    for(int i = 2; i <= n; i++){
+        dp[i] = (dp[i-1]+dp[i-2])%1234567;
     }
-
     return dp[n];
 }
