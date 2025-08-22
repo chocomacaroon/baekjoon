7bd06a5b1a612b6bd3f228c4a6613852a582c523
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-22 17:10:39 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.32 ms, Memory: 4.24 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,48 +1,38 @@
 #include <string>
-#include <vector>
 #include <set>
 #include <algorithm>
-#include <cctype>
+#include <vector>
 
 using namespace std;
 
 int solution(string str1, string str2) {
     int answer = 0;
-    
-    vector<string> a;
-    vector<string> b;
-    
-    for(int i = 0; i < str1.size()-1; i++){
-        char c1 = str1[i];
-        char c2 = str1[i+1];
-        if(isalpha(c1)&&isalpha(c2)){
+    vector<string> s1;
+    vector<string> s2;
+    for(int i = 0; i < str1.size(); i++){
+        if(isalpha(str1[i]) && isalpha(str1[i+1])){
             string s;
-            s += tolower(c1);
-            s += tolower(c2);
-            a.push_back(s);
+            s += tolower(str1[i]);
+            s += tolower(str1[i+1]);
+            s1.push_back(s);
+        }
+            
+    }
+    for(int i = 0; i < str2.size(); i++){
+        if(isalpha(str2[i]) && isalpha(str2[i+1])){
+            string s;
+            s += tolower(str2[i]);
+            s += tolower(str2[i+1]);
+            s2.push_back(s);
         }
     }
-    for(int i = 0; i < str2.size()-1; i++){
-        char c1 = str2[i];
-        char c2 = str2[i+1];
-        if(isalpha(c1)&&isalpha(c2)){
-            string s;
-            s += tolower(c1);
-            s += tolower(c2);
-            b.push_back(s);
-        }
-    }
-    
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    
-    vector<string> v_union;
-    vector<string> v_intersection;
-    
-    set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(v_intersection));
-    set_union(a.begin(), a.end(), b.begin(), b.end(), back_inserter(v_union));
-    if(v_union.size()==0) return 65536;
-    return (int)((double)v_intersection.size() / v_union.size() * 65536);
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    vector<string> result;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(result));
+    vector<string> result2;
+    set_union(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(result2));
+    if(result2.size() == 0) return 65536;
+    return int(result.size()/(double)result2.size()*65536);
+    return answer;
 }
-
-//J(A,B)는 두 집합의 교집합 크기를 두 집합의 합집합 크기로 나눈 값으로 정의된다.
