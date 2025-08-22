b0072d57ad7d686dbb303e3da4a6b15e1ddadd5b
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-08-22 16:07:46 +0900

    [level 2] Title: 롤케이크 자르기, Time: 220.24 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,25 +1,27 @@
 #include <string>
 #include <vector>
-#include <set>
-#include <unordered_map>
+#include <map>
 
 using namespace std;
 
 int solution(vector<int> topping) {
     int answer = 0;
     
-    unordered_map<int, int> left, right;
-    for(int n : topping) right[n]+=1;
-    int lType = 0; 
-    int rType = right.size();
-    for(int i = 0; i < topping.size()-1; i++){
-        left[topping[i]] += 1;
-        right[topping[i]] -= 1;
-        if(left[topping[i]] == 1) lType+=1;
-        if(right[topping[i]] == 0) rType-=1;
-        if(lType == rType) answer+=1;
+    map <int, int> right_counts;
+    for(int t : topping){
+        right_counts[t]++;
+    }
+    map<int, int> left_counts;
+    
+    for(int t : topping){
+        left_counts[t]++;
+        right_counts[t]--;
+        if(right_counts[t] == 0){
+            right_counts.erase(t);
+        }
+        if(left_counts.size() == right_counts.size()){
+            answer++;
+        }
     }
     return answer;
 }
-
-// 
