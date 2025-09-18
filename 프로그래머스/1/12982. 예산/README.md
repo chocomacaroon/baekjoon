e13bb364e19a6a6ec3363d7cb3afa6c00bcd0896
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-09-18 22:25:11 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/README.md
+++ 프로그래머스/1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.01 ms
+메모리: 9.21 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 03일 14:57:23
+2025년 09월 18일 22:25:08
 
 ### 문제 설명
 
--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,9 +1,8 @@
 def solution(d, budget):
-    max = 0
+    answer = 0
     d.sort()
-    for i in range(len(d)+1):
-        for j in range(i+1,len(d)+1):
-            if sum(d[i:j]) <= budget:
-                if len(d[i:j])>=max:
-                    max = len(d[i:j])
-    return max
+    for i in range(len(d)):
+        answer += d[i]
+        if answer > budget:
+            return i
+    return len(d)
