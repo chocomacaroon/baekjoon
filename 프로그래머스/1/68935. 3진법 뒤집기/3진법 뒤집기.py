6ab6566f6f2aaff3a6da24948047ef5d439e7955
권태희 <156282117+chocomacaroon@users.noreply.github.com>
2025-09-18 22:16:41 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,12 +1,10 @@
 def solution(n):
     answer = 0
-    n3 = ""
-    while n//3>0:
-        n3 += str(n%3)
-        n //= 3
-    n3 += str(n%3)
-    n3 = n3[::-1]
-    rev = str(int(n3))
-    for i,c in enumerate(rev):
-        answer += int(c)*(3**i)
+    res = ""
+    while n>0:
+        res += str(n%3)
+        n//=3
+    res = res[::-1]
+    for i in range(len(res)):
+        answer += int(res[i])*(3**i)
     return answer
--- 프로그래머스/1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/1/68935. 3진법 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935?language=python3) 
 
 ### 성능 요약
 
-메모리: 3.85 MB, 시간: 0.64 ms
+메모리: 9.22 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 04일 21:21:19
+2025년 09월 18일 22:16:39
 
 ### 문제 설명
 
